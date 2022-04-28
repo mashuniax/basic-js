32d5ff21069eee28d570a2b2eb760a19eb8678ee
mashuniax <mashuniax@gmail.com>
2022-04-28 17:23:29 +0300

    feat: add tasks sum digits and what season

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (arr.includes('--double-next')) {
+    arr.splice(arr.indexOf('--double-next'), 1, arr[arr.indexOf('--double-next') + 1]);
+    } else if (arr.includes('--double-prev')) {
+    arr.splice(arr.indexOf('--double-prev'), 1, arr[arr.indexOf('--double-prev') - 1]);
+    } else if (arr.includes('--discard-next')) {
+    arr.splice(arr.indexOf('--discard-next'), 2)
+    } else if (arr.includes('--discard-prev')) {
+    arr.splice(arr.indexOf('--discard-prev') - 1, 2)
+    } return arr;
 }
 
 module.exports = {
