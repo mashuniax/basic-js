bdc3dc030cc9600deeae8a24a28b92e8b21f835e
mashuniax <mashuniax1@gmail.com>
2022-04-29 23:41:39 +0300

    fix: fix  advanced sequence interactions in transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,7 +14,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-
   if (!Array.isArray(arr)) {
     throw new Error("'arr' parameter must be an instance of the Array!");
   }
@@ -25,34 +24,43 @@ function transform(arr) {
     return [];
   }
   newArr = arr.slice();
+  if (newArr.includes('--discard-next') && newArr.includes('--double-prev')) {
+  	newArr.splice(newArr.indexOf('--discard-next'), 3)
+  }
+  if (newArr.includes('--discard-next') && newArr.includes('--discard-prev')) {
+  	newArr.splice(newArr.indexOf('--discard-next'), 3)
+  }
   if (newArr.includes('--double-next')) {
     if (newArr[newArr.length - 1] == '--double-next') {
       newArr.splice(newArr.length - 1, 1)
     } else {
       newArr.splice(newArr.indexOf('--double-next'), 1, newArr[newArr.indexOf('--double-next') + 1]);
+    }    
+  }
+  if (newArr.includes('--double-prev')) {
+    if (newArr[0] == '--double-prev') {
+      newArr.splice(0, 1);
+    } else {
+      newArr.splice(newArr.indexOf('--double-prev'), 1, newArr[newArr.indexOf('--double-prev') - 1]);
+    }
+  } 
+  if (newArr.includes('--discard-next')) {
+    if (newArr[0] == '--discard-next') {
+      newArr.splice(newArr.length - 1, 1)
+    } else {
+      newArr.splice(newArr.indexOf('--discard-next'), 2)
     }
     
-    } else if (newArr.includes('--double-prev')) {
-      if (newArr[0] == '--double-prev') {
-        newArr.splice(0, 1);
-      } else {
-        newArr.splice(newArr.indexOf('--double-prev'), 1, newArr[newArr.indexOf('--double-prev') - 1]);
-      }
-    } else if (newArr.includes('--discard-next')) {
-      if (newArr[0] == '--discard-next') {
-        newArr.splice(newArr.length - 1, 1)
-      } else {
-        newArr.splice(newArr.indexOf('--discard-next'), 2)
-      }
-     
-    } else if (newArr.includes('--discard-prev')) {
-    	if (newArr[0] == '--discard-prev') {
-      	newArr.splice(0, 1)
-      } else {
-      	newArr.splice(newArr.indexOf('--discard-prev') - 1, 2)
-      }
-      
-    } return newArr;
+  }
+  if (newArr.includes('--discard-prev')) {
+    if (newArr[0] == '--discard-prev') {
+      newArr.splice(0, 1)
+    } else {
+      newArr.splice(newArr.indexOf('--discard-prev') - 1, 2)
+    }
+    
+  } 
+  return newArr;
 }
 
 module.exports = {
