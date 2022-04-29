e0a1a0246c83337c6ec5b685f52453911f0eb38c
mashuniax <mashuniax1@gmail.com>
2022-04-30 00:15:48 +0300

    feat: add dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -15,15 +15,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function createDreamTeam(members) {
   let arr = [];
-  if (members.length == null) {
+
+  if (!Array.isArray(members)) {
     return false 
   }
+  // for (let i = 0; i < members.length; i++) {
+  //   members[i] = members[i].trim();
+  // }
   for(let i = 0; i < members.length; i++) {
     if (typeof(members[i]) == 'string') {
+      members[i] = members[i].trim();
       arr.push(members[i])
     }
   }
-  return arr.map(item => item[0]).sort().join('').toUpperCase();
+  return arr.map(item => item[0]).join('').toUpperCase().split('').sort().join('');
 }
 
 module.exports = {
