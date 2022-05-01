313a8830f70609585656c7327f8de687f9073776
mashuniax <mashuniax1@gmail.com>
2022-05-02 01:23:04 +0300

    feat: add extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+	let arr = [];
+	if (typeof str !== 'string') {
+  	str = String(str);
+  }
+  if (typeof options.addition !== 'string') {
+  	options.addition = String(options.addition);
+  }
+  
+  if (options.repeatTimes !== undefined) {
+  	for (let i = 0; i < options.repeatTimes; i++) {
+    	arr.push(str)
+    }
+  } else {
+  	arr.push(str)
+  }
+  
+  let addArr = [];
+  if (options.addition !== undefined) {
+    if (options.additionRepeatTimes !== undefined) {
+      for (let i = 0; i < options.additionRepeatTimes; i++) {
+        addArr.push(options.addition)
+      }
+    } else {
+      addArr.push(options.addition)
+
+    }
+    
+    if (options.additionSeparator !== undefined) {
+      for (let i = 0; i < addArr.length - 1; i++) {
+        addArr[i] = addArr[i] + options.additionSeparator;
+      }
+    } else {
+        for (let i = 0; i < addArr.length - 1; i++) {
+        addArr[i] = addArr[i] + '|';
+      }
+    }
+    let addStr = addArr.join('');
+    console.log(addStr)
+    	if (addStr !== 'undefined') {
+       	for (let i = 0; i < arr.length; i++) {
+      	arr[i] = arr[i] + addStr;
+    	}
+    }
+
+  }
+
+
+	if (options.separator !== undefined) {
+		for (let i = 0; i < arr.length - 1; i++) {
+    	arr[i] = arr[i] + options.separator;
+    }
+  } else {
+  		for (let i = 0; i < arr.length - 1; i++) {
+    	arr[i] = arr[i] + '+';
+    }
+  }
+  return arr.join('');
 }
 
 module.exports = {
